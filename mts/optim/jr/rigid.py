fix merging tracks & jacobians for points and poses

mts/optim/jr/rigid.py
@@ -2,6 +2,8 @@ from typing import NamedTuple
 
 import numpy as np
 
+from mts.optim.jr.base import JacobianResiduals
+
 from ..gaussnewton import BaseJR, ToSecondOrderMixin
 
 from mts.pose.rigid import Rigid3D
@@ -28,7 +30,6 @@ class PoseJR(ToSecondOrderMixin, BaseJR[Rigid3D, PoseParam]):
             param.world_points,
         )
         cost = np.linalg.norm(residuals, axis=1).mean()
-        jacobians = jacobians.transpose(2, 0, 1)
         return jacobians, residuals, cost
 
     def update_term(
@@ -49,11 +50,11 @@ class _jr:
         pose: Rigid3D,
         image_points: np.ndarray,
         world_points: np.ndarray,
-    ):
+    ) -> JacobianResiduals:
         camera_points = pose * world_points
         return self.in_camera(K, image_points, camera_points)
 
-    def in_camera(self, K, image_points: np.ndarray, camera_points):
+    def in_camera(self, K, image_points: np.ndarray, camera_points,) -> JacobianResiduals:
         fx, fy = K[0, 0], K[1, 1]
         cx, cy = K[0, 2], K[1, 2]
 
@@ -94,7 +95,8 @@ class _jr:
             ]
         )
         es = image_points - projected_points
-        return js, es
+        js = js.transpose(2, 0, 1)
+        return JacobianResiduals(js, es)
 
 
 jr = _jr()

mts/pose/rigid.py
@@ -8,7 +8,6 @@ from tqdm.auto import tqdm
 
 from mts.geometry.transform import view_from_Rt
 from mts.geometry.triangulation import linear
-from mts.optim.jr.rigid import jr
 from mts.types import NPMatrix3x3f, NPMatrix4x4f, NPVector3f
 from mts.util.np import add_col
 
@@ -194,60 +193,59 @@ def compute_pose(E, st_points, nd_points):
     return best_R, best_t, best_point_3D, best_mask
 
 
-def gd_pnp(
-    K,
-    pose: Rigid3D,
-    image_points: np.ndarray,
-    world_points: np.ndarray,
-    iterations: int = 100,
-    learning_rate: float = 0.000001,
-    cost_th: float = 0.02,
-):
-    last_cost = np.inf
-    with tqdm(total=iterations) as tbar:
+# def gd_pnp(
+#     K,
+#     pose: Rigid3D,
+#     image_points: np.ndarray,
+#     world_points: np.ndarray,
+#     iterations: int = 100,
+#     learning_rate: float = 0.000001,
+#     cost_th: float = 0.02,
+# ):
+#     last_cost = np.inf
+#     with tqdm(total=iterations) as tbar:
 
-        for _ in range(iterations):
-            js, es = jr(K, pose, image_points, world_points)
-            dxs = -(js.transpose(2, 1, 0) @ es[:, :, np.newaxis]).squeeze()
-            dx = dxs.mean(0)
-            dx = learning_rate * dx
+#         for _ in range(iterations):
+#             js, es = jr(K, pose, image_points, world_points)
+#             dxs = -(js.transpose(2, 1, 0) @ es[:, :, np.newaxis]).squeeze()
+#             dx = dxs.mean(0)
+#             dx = learning_rate * dx
 
-            cost = np.linalg.norm(es, axis=1).mean()
-            if cost - cost_th > last_cost:
-                break
-            last_cost = cost
-            tbar.set_postfix({"cost": cost})
-            tbar.update()
+#             cost = np.linalg.norm(es, axis=1).mean()
+#             if cost - cost_th > last_cost:
+#                 break
+#             last_cost = cost
+#             tbar.set_postfix({"cost": cost})
+#             tbar.update()
 
-            pose = Rigid3D.Exp(dx) * pose
-    return pose
+#             pose = Rigid3D.Exp(dx) * pose
+#     return pose
 
 
-def gn_pnp(
-    K,
-    pose: Rigid3D,
-    image_points: np.ndarray,
-    world_points: np.ndarray,
-    iterations: int = 100,
-    cost_th: float = 0.02,
-):
-    last_cost = np.inf
-    with tqdm(total=iterations) as tbar:
-        for _ in range(iterations):
-            js, es = jr(K, pose, image_points, world_points)
-            Hs = js.transpose(2, 1, 0) @ js.transpose(2, 0, 1)
-            dxs = -(js.transpose(2, 1, 0) @ es[:, :, np.newaxis]).squeeze()
-            H = Hs.sum(0)
-            dx = dxs.sum(0)
-            dx = np.linalg.solve(H, dx)
-            cost = np.linalg.norm(es, axis=1).mean()
-            if cost - cost_th > last_cost:
-                break
-            last_cost = cost
+# def gn_pnp(
+#     K,
+#     pose: Rigid3D,
+#     image_points: np.ndarray,
+#     world_points: np.ndarray,
+#     iterations: int = 100,
+#     cost_th: float = 0.02,
+# ):
+#     last_cost = np.inf
+#     with tqdm(total=iterations) as tbar:
+#         for _ in range(iterations):
+#             js, es = jr(K, pose, image_points, world_points)
+#             Hs = js.transpose(2, 1, 0) @ js.transpose(2, 0, 1)
+#             dxs = -(js.transpose(2, 1, 0) @ es[:, :, np.newaxis]).squeeze()
+#             H = Hs.sum(0)
+#             dx = dxs.sum(0)
+#             dx = np.linalg.solve(H, dx)
+#             cost = np.linalg.norm(es, axis=1).mean()
+#             if cost - cost_th > last_cost:
+#                 break
+#             last_cost = cost
 
-            tbar.set_postfix({"cost": cost})
-            tbar.update()
+#             tbar.set_postfix({"cost": cost})
+#             tbar.update()
 
-            pose = Rigid3D.Exp(dx) * pose
-    return pose
-
+#             pose = Rigid3D.Exp(dx) * pose
+#     return pose

mts/sfm/incremental.py
@@ -7,7 +7,7 @@ from typing import Dict, Generator, List, Optional, Tuple, Union
 
 import cv2
 import numpy as np
-from tqdm.auto import tqdm
+from tqdm import tqdm
 
 from mts.estimator.pnp.dlt import LinearPNPRansac
 from mts.geometry.triangulation import linear
@@ -16,10 +16,14 @@ from mts.model.image import Image, ImageId, ImageType
 from mts.model.point import Point3D, Point3Df
 from mts.model.rgb import RGB
 from mts.model.two_view import PairId, TwoViewPair, compute_two_view
-from mts.pose.rigid import Rigid3D, gn_pnp
+from mts.optim.jr.point import PointParam
+from mts.optim.jr.rigid import PoseParam
+from mts.optim.nonlinear.rigid import gn_pose
+from mts.pose.rigid import Rigid3D
 from mts.sfm.reconstruction import Reconstruction
-from mts.sfm.scene_graph import SceneGraph, View3DPair
+from mts.sfm.scene_graph import ReconstructedPoint, SceneGraph, View3DPair
 from mts.types import NPVector2f, NPVector3f
+from mts.optim.nonlinear.point import gn_point
 
 LOGGER = logging.getLogger(__name__)
 
@@ -136,7 +140,7 @@ class IncrementalSfM:
         self.min_depth = min_depth
         self.max_depth = max_depth
         self.matcher = matcher
-        self.reconstructions = []
+        self.reconstructions: Reconstruction = []
 
     def _compute_scene_graph(self):
         self.scene_graph = SceneGraph(self.images_map)
@@ -171,26 +175,7 @@ class IncrementalSfM:
     def _check_depth_interval(self, pose: Rigid3D, point3d: NPVector3f) -> bool:
         return self.min_depth <= pose.z_of(point3d) <= self.max_depth
 
-    def _refine_points_3d(self, two_view_pair: TwoViewPair) -> None:
-
-        new_points = []
-        for world_point, st_image_point, nd_image_point in zip(
-            tqdm(two_view_pair.points3D, desc="Refine initial 3D points"),
-            two_view_pair.st_keypoints_camera,
-            two_view_pair.nd_keypoints_camera,
-        ):
-            new_point = non_linear(
-                [two_view_pair.st_image.pose, two_view_pair.nd_image.pose],
-                world_point,
-                [st_image_point[:2], nd_image_point[:2]],
-                max_iteration=500,
-            )
-
-            new_points.append(new_point)
-
-        two_view_pair.points3D = np.stack(new_points)
-
-    def init_3d_points(self, two_view_pair: TwoViewPair) -> None:
+    def init_3d_points(self, two_view_pair: TwoViewPair) -> list[ReconstructedPoint]:
 
         st_image = two_view_pair.st_image
         nd_image = two_view_pair.nd_image
@@ -227,20 +212,18 @@ class IncrementalSfM:
             )
 
             points[st_track_idx] = self.scene_graph.points[st_track_idx]
-        return points
+        return list(points.values())
 
     def _start_from_pair(self, pair: TwoViewPair) -> None:
         self.scene_graph.images[pair.st_image.image_id].pose = Rigid3D.from_identity()
         self.scene_graph.images[pair.nd_image.image_id].pose = pair.relative_pose
         initial_3d_points = self.init_3d_points(pair)
 
-        reconstruction = Reconstruction(
-            images=[
-                pair.st_image,
-                pair.nd_image,
-            ],
-            points=initial_3d_points,
-        )
+        reconstruction = Reconstruction(self.scene_graph)
+        reconstruction.add_image(pair.st_image)
+        reconstruction.add_image(pair.nd_image)
+        reconstruction.add_points(initial_3d_points)
+
         LOGGER.info(
             "Starting from pair: (%d, %d) with angle (%f)",
             pair.st_image.image_id,
@@ -427,7 +410,7 @@ class IncrementalSfM:
                 reconstructed_point.error = error
                 reconstructed_point.color = RGB(*image.img[int(j), int(i)])
                 if inv_track.idx not in reconstruction.points:
-                    reconstruction.points[inv_track.idx] = reconstructed_point
+                    reconstruction.add_point(reconstructed_point)
                     added_new_points += 1
 
         LOGGER.info("Add new %d points", added_new_points)
@@ -488,23 +471,34 @@ class IncrementalSfM:
             )
             if image.pose is None:
                 continue
-            pose = gn_pnp(
-                image.K,
+
+            pose = gn_pose(
                 image.pose,
-                image_points,
-                world_points,
-                iterations=1000,
+                PoseParam(
+                    image.K,
+                    image_points,
+                    world_points,
+                ),
+                2000,
+                0.02,
             )
             image.pose = pose
 
-        LOGGER.info("refine the point")
-        for reconstructed_point in reconstruction.points.values():
-            new_point = non_linear(
-                reconstructed_point.poses(),
+        LOGGER.info("refine the points")
+        for reconstructed_point in tqdm(
+            reconstruction.points.values(),
+            total=len(reconstruction.points),
+        ):
+            new_point = gn_point(
                 reconstructed_point.loc.as_np(),
-                reconstructed_point.kp_camera(),
-                scaler=0.002,
-                max_iteration=100,
+                PointParam(
+                    [image.K for image in reconstructed_point.images()],
+                    reconstructed_point.kp_image(),
+                    reconstructed_point.poses(),
+                ),
+                iterations=100,
+                cost_threshold=0.02,
+                verbose=False,
             )
             reconstructed_point.loc.x = new_point[0]
             reconstructed_point.loc.y = new_point[1]
@@ -547,6 +541,8 @@ class IncrementalSfM:
                 "Refine triangulation using non-linear triangulation",
             )
             self._refine_non_linear(self.reconstructions[-1])
+
+        self._refine_non_linear(self.reconstructions[-1])
 
     @classmethod
     def from_config(

mts/sfm/reconstruction.py
@@ -1,69 +1,55 @@
 from dataclasses import dataclass, field
 from typing import Dict, List
+from collections import OrderedDict
 
-import numpy as np
 
 from mts.model.image import Image
-from mts.model.point import Point3D
-from mts.model.rgb import RGB
-from mts.model.track import KeypointTrack
-from mts.sfm.scene_graph import SceneGraph
-from mts.types import Id
+from mts.sfm.scene_graph import ReconstructedPoint, SceneGraph
 
 
-@dataclass
-class ReconstructedPoint:
-    id: Id = field(default=None)
-    loc: Point3D = field(default=None)
-    color: RGB = field(default=None)
-    track: KeypointTrack = field(default=None)
-    error: float = field(default=np.inf)
-    scene_graph: SceneGraph = field(default=None, repr=False)
-
-    def poses(self, not_none: bool = True):
-        return [
-            tracklet.image.pose
-            for tracklet in self.track
-            if not not_none or tracklet.image.pose is not None
-        ]
-
-    def kp_image(self, not_none: bool = True):
-        return [
-            tracklet.image_kp
-            for tracklet in self.track
-            if not not_none or tracklet.image.pose is not None
-        ]
-
-    def images(self, not_none: bool = True):
-        return [
-            tracklet.image
-            for tracklet in self.track
-            if not not_none or tracklet.image.pose is not None
-        ]
-
-    def kp_camera(self, not_none: bool = True, homogenous: bool = False):
-        homogenous_slice = slice(None, -1)
-        if homogenous:
-            homogenous_slice = slice(None, 2)
-        if not_none:
-            return [
-                tracklet.camera_kp[homogenous_slice]
-                for tracklet in self.track
-                if tracklet.image.pose is not None
-            ]
-        if homogenous:
-            return [tracklet.camera_kp[homogenous_slice] for tracklet in self.track]
-
-
-@dataclass
+@dataclass(init=False)
 class Reconstruction:
+    _scene_graph: SceneGraph = field()
 
     images: List[Image] = field(default_factory=list)
-    points: Dict[int, ReconstructedPoint] = field(default=None)
+    points: OrderedDict[int, ReconstructedPoint] = field(default=None)
+
+    def __init__(self, scene_graph: SceneGraph):
+        self._scene_graph = scene_graph
+        self.points = {}
+        self.images = []
+        self._point_at_map = {}
 
     @property
     def images_ids(self):
         return {image.image_id for image in self.images}
+
+    def add_point(self, reconstructed_point: ReconstructedPoint) -> None:
+        self.points[reconstructed_point.id] = reconstructed_point
+        self._point_at_map[reconstructed_point.id] = len(self.points)
+
+    def add_points(self, reconstructed_points: list[ReconstructedPoint]) -> None:
+        for idx, reconstructed_point in enumerate(reconstructed_points, len(self.points)):
+            self.points[reconstructed_point.id] = reconstructed_point
+            self._point_at_map[reconstructed_point.id] = idx
+
+    def visible_point_for(self, image: Image) -> list[ReconstructedPoint]:
+        visible_points = []
+        for inv_tracklet in self._scene_graph.keypoint_invtracks[image.image_id].values():
+            reconstructed_point = self.points.get(inv_tracklet.idx)
+            if reconstructed_point is not None:
+                visible_points.append(reconstructed_point)
+
+        return visible_points
+
+    def visible_point_idxs(self, image: Image) -> list[int]:
+        visible_point_idxs = []
+        for inv_tracklet in self._scene_graph.keypoint_invtracks[image.image_id].values():
+            idx = self._point_at_map.get(inv_tracklet.idx)
+            if idx is not None:
+                visible_point_idxs.append(idx)
+
+        return visible_point_idxs
 
     def add_image(self, image: Image):
         self.images.append(image)

mts/sfm/scene_graph.py
@@ -9,14 +9,17 @@ import numpy as np
 
 from mts.estimator.pnp.dlt import compute_projection_error
 from mts.model.image import Image, ImageId, ImageType
+from mts.model.point import Point3D
+from mts.model.rgb import RGB
 from mts.model.track import (
     InvTrack,
     KeypointInvTrack,
+    KeypointTrack,
     TrackLocation,
 )
 from mts.model.two_view import PairId, TwoViewPair, image_ids_to_pair_id
-from mts.sfm.reconstruction import ReconstructedPoint
-from mts.types import NPVector3f
+
+from mts.types import Id, NPVector3f
 
 LOGGER = logging.getLogger("SfM")
 
@@ -33,6 +36,50 @@ class View3DPair(NamedTuple):
 
 
 @dataclass
+class ReconstructedPoint:
+    id: Id = field(default=None)
+    loc: Point3D = field(default=None)
+    color: RGB = field(default=None)
+    track: KeypointTrack = field(default=None)
+    error: float = field(default=np.inf)
+    scene_graph: SceneGraph = field(default=None, repr=False)
+
+    def poses(self, not_none: bool = True):
+        return [
+            tracklet.image.pose
+            for tracklet in self.track
+            if not not_none or tracklet.image.pose is not None
+        ]
+
+    def kp_image(self, not_none: bool = True):
+        return [
+            tracklet.image_kp
+            for tracklet in self.track
+            if not not_none or tracklet.image.pose is not None
+        ]
+
+    def images(self, not_none: bool = True):
+        return [
+            tracklet.image
+            for tracklet in self.track
+            if not not_none or tracklet.image.pose is not None
+        ]
+
+    def kp_camera(self, not_none: bool = True, homogenous: bool = False):
+        homogenous_slice = slice(None, -1)
+        if homogenous:
+            homogenous_slice = slice(None, 2)
+        if not_none:
+            return [
+                tracklet.camera_kp[homogenous_slice]
+                for tracklet in self.track
+                if tracklet.image.pose is not None
+            ]
+        if homogenous:
+            return [tracklet.camera_kp[homogenous_slice] for tracklet in self.track]
+
+
+@dataclass
 class SceneGraph:
     images: Dict[ImageId, Image]
     pairs: Optional[Dict[PairId, TwoViewPair]] = field(default_factory=dict)
@@ -46,7 +93,7 @@ class SceneGraph:
         init=False,
         default_factory=dict,
     )
-    _last_track_id: int = field(default=-1, init=False)
+    _last_point_id: int = field(default=-1, init=False)
 
     def __post_init__(self):
         for pair in self.pairs.values():
@@ -105,7 +152,9 @@ class SceneGraph:
         image_id = self._extract_image_id(image_id)
         return self.pairs_map[image_id]
 
-    def add_two_view_pair(self, two_view_pair: TwoViewPair, strict: bool = True) -> None:
+    def add_two_view_pair(
+        self, two_view_pair: TwoViewPair, strict: bool = True
+    ) -> None:
         if two_view_pair.pair_id in self.pairs and strict:
             raise ValueError(
                 f"pair id `{two_view_pair.pair_id}` already exists in the scene graph"
@@ -217,6 +266,7 @@ class SceneGraph:
             else:
                 if st_inv_track.idx == nd_inv_track.idx:
                     continue
+                # TODO: check for inconsistencies
                 self._merge_tracks(st_inv_track, nd_inv_track)
 
     def _create_track(
@@ -228,21 +278,43 @@ class SceneGraph:
         st_kp_mask,
         nd_kp_mask,
     ) -> None:
-        self._last_track_id += 1
+        self._last_point_id += 1
 
         reconstructed_point = ReconstructedPoint(
-            id=self._last_track_id,
+            id=self._last_point_id,
             track=[
                 TrackLocation(st_image, st_match),
                 TrackLocation(nd_image, nd_match),
             ],
             scene_graph=self,
         )
-        self.points[self._last_track_id] = reconstructed_point
-        st_kp_mask[st_match] = InvTrack(self._last_track_id, 0)
-        nd_kp_mask[nd_match] = InvTrack(self._last_track_id, 1)
+        self.points[self._last_point_id] = reconstructed_point
+        st_kp_mask[st_match] = InvTrack(self._last_point_id, 0)
+        nd_kp_mask[nd_match] = InvTrack(self._last_point_id, 1)
 
-    def _merge_tracks(self, st_inv_track: InvTrack, nd_inv_track: InvTrack) -> None:
+    def _can_merge(
+        self,
+        st_inv_track: InvTrack,
+        nd_inv_track: InvTrack,
+    ) -> bool:
+        st_image_tracks = {
+            tracklet.image.image_id: tracklet.keypoint_num
+            for tracklet in self.points[st_inv_track.idx].track
+        }
+        for tracklet in self.points[nd_inv_track.idx].track:
+            st_image_kp = st_image_tracks.get(tracklet.image.image_id)
+            if st_image_kp is not None:
+                if st_image_kp != tracklet.keypoint_num:
+                    return False
+        return True
+
+    def _merge_tracks(
+        self,
+        st_inv_track: InvTrack,
+        nd_inv_track: InvTrack,
+    ) -> None:
+        if not self._can_merge(st_inv_track, nd_inv_track):
+            return
         reconstructed_point = self.points.pop(nd_inv_track.idx)
         starting_len = len(self.points[st_inv_track.idx].track)
         for num, tracklet in enumerate(reconstructed_point.track, starting_len):